7e49a72fdd1c937cf22c67bf830e3dff6f168c31
Arkit <arkit312003@gmail.com>
2025-08-01 22:42:51 +0000

    completed project ready to shoot

---
+++ src/langgraphagenicai/nodes/basic_chatbot_node.py
@@ -0,0 +1,15 @@
+from src.langgraphagenicai.state.state import State
+
+
+class BasicChatbot:
+    """
+    Basic Chatbot logic implementation
+    """
+    def __init__(self,model):
+        self.llm=model
+        
+    def process(self,state:State)->dict:
+        """
+        Process a input state and generates a chatbot response.
+        """
+        return {"messages":self.llm.invoke(state['messages'])}
